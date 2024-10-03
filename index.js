a7b30177c46c86fa79b3c14fbce9c48d87264ef9
Mmathabo Mogane <mmathaboappreciate@gmail.com>
2024-10-03 12:16:19 +0200

    import initial data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TASK: import helper functions from utils
+import utils from "./taskFunction.js";
 // TASK: import initialData
+import initialData from "./initialData.js";
 
 
 /*************************************************************************************************************************************************
